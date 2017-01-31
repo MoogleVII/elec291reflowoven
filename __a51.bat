b348ecd478d77afd5acc791ffb86d3cc406c36d7
Justin <justin.art.lane@gmail.com>
2017-01-31 10:16:02 -0800

    Added init files to main branch

---
+++ __a51.bat
@@ -0,0 +1,5 @@
+@echo off
+D:
+cd "\School\ELEC 291\lab3\"
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "D:\School\ELEC 291\lab3\Temp_monitor.asm"
+echo Crosside_Action Set_Hex_File D:\School\ELEC 291\lab3\Temp_monitor.HEX
