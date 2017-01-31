2025189af70bcaecd71b7383b4b83bed864883a7
MoogleVII <justin.art.lane@gmail.com>
2017-01-31 10:06:24 -0800

    All my lab 3 files
    Need to use math32.inc in this folder or it won't work

---
+++ lab3 files/Stripchart_generator_temperature_monitor.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import sys, time, math
+import serial
+
+ser = serial.Serial(
+    port='COM4',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS
+)
+ser.isOpen()
+    
+xsize=50
+   
+def data_gen():
+    t = data_gen.t
+    while True:
+       t+=1
+       val=ser.readline()
+       yield t, val
+
+def run(data):
+    # update the data
+    t,y = data
+    if t>-1:
+        xdata.append(t)
+        ydata.append(y)
+        if t>xsize: # Scroll to the left.
+            ax.set_xlim(t-xsize, t)
+        line.set_data(xdata, ydata)
+
+    return line,
+
+def on_close_figure(event):
+    sys.exit(0)
+
+data_gen.t = -1
+fig = plt.figure()
+fig.canvas.mpl_connect('close_event', on_close_figure)
+ax = fig.add_subplot(111)
+line, = ax.plot([], [], lw=2)
+ax.set_ylim(0, 50)
+ax.set_xlim(0, 50)
+ax.grid()
+xdata, ydata = [], []
+
+# Important: Although blit=True makes graphing faster, we need blit=False to prevent
+# spurious lines to appear when resizing the stripchart.
+ani = animation.FuncAnimation(fig, run, data_gen, blit=False, interval=10, repeat=False)
+plt.show()
